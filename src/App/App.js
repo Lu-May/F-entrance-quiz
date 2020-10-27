b9f480434551d4d5539a184d97ee10b09851f531
Lu-May <1054998345@qq.com>
2020-10-27 23:10:06 +0800

    [luyuehuan] feat:add group button

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
+import GroupList from './components/GroupList/GroupList';
 import StudentList from './components/StudentList/StudentList';
 
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
+        <GroupList />
         <StudentList />
       </div>
     );
